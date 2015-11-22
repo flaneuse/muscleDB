f58188394fa29ceef81d3d42692129ddbbd3caf1
flaneuse <laura.d.hughes@gmail.com>
2015-11-22 16:38:55 -0500

    switched to rds read

--- filterExpr.r
+++ filterExpr.r
@@ -2,57 +2,55 @@
 # the inputs change.
 filterData <- reactive({
   
+  # Number to return at a specific time.
   num2Return = 2500
   
   x = proc.time()
   
-
-# #   
-#   
-#   # Per1, Per2, Per3, ....
-#   # Note: to change to exact matching, include '$' at the end of the string.
-#   # geneInput = paste0('^',input$geneInput) # Antiquated; for 
+  
+  #   # Per1, Per2, Per3, ....
+  #   # Note: to change to exact matching, include '$' at the end of the string.
+  #   # geneInput = paste0('^',input$geneInput) # Antiquated; for 
   geneInput = paste0(input$geneInput, '%')
   ont = paste0('%', input$GO, '%')
-#   
-#   
-#   # SELECT DATA.
-#   # Note: right now, if there's something in both the "gene" and "ont"
-#   # input boxes, they must BOTH be true (AND relationship).
-#   # For example, if you have gene = "Myod1" and ont = "kinase",
-#   # you'll find only genes w/ both the name Myod1 and kinase as an ontology (which doesn't exist).
-#   # To switch this to an OR relationship, change the '&' to a '|'.
-#   
-#   # filtered = semi_join(mt, df2, copy = TRUE, auto_index = TRUE) %>% 
+  #   
+  #   
+  #   # SELECT DATA.
+  #   # Note: right now, if there's something in both the "gene" and "ont"
+  #   # input boxes, they must BOTH be true (AND relationship).
+  #   # For example, if you have gene = "Myod1" and ont = "kinase",
+  #   # you'll find only genes w/ both the name Myod1 and kinase as an ontology (which doesn't exist).
+  #   # To switch this to an OR relationship, change the '&' to a '|'.
+  #   
+  #   # filtered = semi_join(mt, df2, copy = TRUE, auto_index = TRUE) %>% 
   filtered = data %>% 
-    filter(tissue %in% input$muscles,
-           transcript %like% geneInput
+    filter(tissue %in% input$muscles
+           # transcript %like% geneInput
            # , GO %like% ont
     )
   
   
   
-  head(filtered, n = num2Return)
   # filter(filtered, row_number(transcript) == 1L)
-
-#   
-#   # Quantitative filtering
-#   filteredTranscripts = filtered %>% 
-#     filter(expr <= input$maxExprVal, 
-#            expr >= input$minExprVal, 
-#            q <= input$qVal) %>% 
-#     select(transcript)
-#   
-#   # Select the transcripts where at least one tissue meets the conditions.
-#   filtered = left_join(filtered, filteredTranscripts)
-#   
-#   print(proc.time() - x)
-#   
-#   return(filtered)
-#   #       if (input$saveRowsTable > 0){
-#   #         filtered = filtered %>% 
-#   #           slice(as.numeric(input$table_rows_selected))
-#   #       } else {
-#   #         filtered
-#   #       }
+  
+  #   
+  #   # Quantitative filtering
+  #   filteredTranscripts = filtered %>% 
+  #     filter(expr <= input$maxExprVal, 
+  #            expr >= input$minExprVal, 
+  #            q <= input$qVal) %>% 
+  #     select(transcript)
+  #   
+  #   # Select the transcripts where at least one tissue meets the conditions.
+  #   filtered = left_join(filtered, filteredTranscripts)
+  #   
+  #   print(proc.time() - x)
+  #   
+  #   return(filtered)
+  #   #       if (input$saveRowsTable > 0){
+  #   #         filtered = filtered %>% 
+  #   #           slice(as.numeric(input$table_rows_selected))
+  #   #       } else {
+  #   #         filtered
+  #   #       }
 })
--- global.R
+++ global.R
@@ -10,6 +10,11 @@ library(ggplot2)
 
 # Import in the Muscle Transcriptome database -----------------------------
 
-mt_source = src_sqlite('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_public_2015-11-08.sqlite3', create = FALSE)
-data = tbl(mt_source, 'MT')
+# mt_source = src_sqlite('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_public_2015-11-08.sqlite3', create = FALSE)
+# data = tbl(mt_source, 'MT')
+
+data = readRDS('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_public_2015-11-08.rds')
+
+data = data %>% 
+  slice(1:2500)
 maxInit = 27000
