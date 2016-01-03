ae50b65da3ca14316d3f7e3a91e227bdd811fb99
flaneuse <laura.d.hughes@gmail.com>
2016-01-03 11:21:12 -0500

    render volcanoTable

--- volcano.R
+++ volcano.R
@@ -1,7 +1,18 @@
-output$volcano <- renderDataTable({
+output$volcanoTable <- renderDataTable({
   filtered = filterData()
   
-})
+},  
+escape = c(-1,-2, -3),
+options = list(searching = TRUE, stateSave = TRUE,
+               pageLength = 25,
+               rowCallback = JS(
+                 'function(nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+        if (aData[0])
+          $("td:eq(0)", nRow).css("color", "#293C97");
+          $("td", nRow).css("text-align", "center");
+      }')
+)
+)
 
 # plotVolcano <- reactive({
 #   xLab = filterVolcano()$xLab[1]
