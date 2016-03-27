2d7493e09d04618ad5af310cc91762c9d79f1655
flaneuse <laura.d.hughes@gmail.com>
2016-03-27 16:47:54 -0400

    update

--- comparison.R
+++ comparison.R
@@ -1,24 +1,24 @@
-comparison = reactive({
-  filteredData = filterData()
-
-  transcriptList = c('uc033fhy', 'uc007afa')
-  
-  filteredData = data %>% 
-    filter(transcript %in% transcriptList) %>% 
-    mutate(expr2 = ifelse(transcript %in% transcriptList[1], -1 * expr, expr))
-
-  # Temporary plgot to be replaced by interactive version.
-  filteredData %>%   
-    group_by(transcript) %>% 
-    ggvis(x = ~tissue, y = ~expr2, colour =: ~transcript) %>%
-    layer_bars()
-})%>% bind_shiny("compPlot")
-
+# comparison = reactive({
+#   filteredData = filterData()
 # 
-# mtcars %>%   ggvis(~mpg, ~wt) %>%
-#   layer_points() %>%
-#   layer_smooths() %>% 
-#   bind_shiny("compPlot")
+#   transcriptList = c('uc033fhy', 'uc007afa')
+#   
+#   filteredData = data %>% 
+#     filter(transcript %in% transcriptList) %>% 
+#     mutate(expr2 = ifelse(transcript %in% transcriptList[1], -1 * expr, expr))
+# 
+#   # Temporary plgot to be replaced by interactive version.
+#   filteredData %>%   
+#     group_by(transcript) %>% 
+#     ggvis(x = ~tissue, y = ~expr2, colour =: ~transcript) %>%
+#     layer_bars()
+# })%>% bind_shiny("compPlot")
+# 
+# # 
+mtcars %>%   ggvis(~mpg, ~wt) %>%
+  layer_points() %>%
+  layer_smooths() %>%
+  bind_shiny("compPlot")
 
 
 # n= n %>% mutate(expr1 = n/maxN)
