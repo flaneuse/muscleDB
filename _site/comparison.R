cb7496ee609f650415d0167bdb05d68754aeddd2
flaneuse <laura.d.hughes@gmail.com>
2016-03-27 14:40:51 -0400

    update

--- _site/comparison.R
+++ _site/comparison.R
@@ -1,12 +1,12 @@
-comparison = reactive({
-  filteredData = filterData()
-
-
-  # Temporary plot to be replaced by interactive version.
-  mtcars %>%   ggvis(~mpg, ~wt) %>%
-    layer_points() %>%
-    layer_smooths()
-})%>% bind_shiny("compPlot")
+# reactive({
+#   filteredData = filterData()
+#   
+#   
+#   # Temporary plot to be replaced by interactive version.  
+#   mtcars %>%   ggvis(~mpg, ~wt) %>%
+#     layer_points() %>%
+#     layer_smooths()
+# })%>% bind_shiny("compPlot")
 
 
 mtcars %>%   ggvis(~mpg, ~wt) %>%
