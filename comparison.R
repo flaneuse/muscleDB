45c52e1663cccc17667c3c93db0b758218ae85f1
flaneuse <laura.d.hughes@gmail.com>
2016-04-11 20:46:43 -0400

    basic comp plot with ref gene selection

--- comparison.R
+++ comparison.R
@@ -1,128 +1,92 @@
-output$compPlot = renderPlotly({
-
-  filteredData = filterData()
+# Gene selection box --------------------------------------------------
+output$g1 = renderUI({
   
-  refGene = filteredData$transcript[1]
+  # Pull out the names of the filtered genes.
+  selGenes = filterData() %>% 
+    mutate(fullName = paste0(gene, ' (', transcript, ')'))
+  
+  selGenes = unique(as.character(selGenes$fullName)) # shortName is factorized...
+  
+  selectizeInput('compRef', label = 'ref. transcript',
+                 choices = selGenes,
+                 width = '200px')
+})
+
+
+
+# Comparison plot  ------------------------------------------------------
+
+output$compPlot = renderPlot({
+  
+  
+  # filter data
+  filteredData = filterData() %>% 
+    mutate(fullName = paste0(gene, ' (', transcript, ')'))
+  
+  
+  # pull out the data for the reference gene
+  refGene = input$compRef
   
   refExpr = filteredData %>% 
-    filter(transcript == refGene) %>% 
+    filter(fullName == refGene) %>% 
     mutate(refExpr = expr) %>% 
     select(tissue, refExpr)
   
-  
-  filteredData = left_join(filteredData, refExpr, by = 'tissue')
-  
-  
-  pairwise = spread(filteredData %>% select(tissue, transcript, expr, refExpr),
-                    transcript, expr) %>% 
-    select(-tissue)
-  
-  correl = data.frame(cor(pairwise)) %>% 
-    select(corr = refExpr) 
-  
-  correl = correl %>%
-    mutate(transcript = row.names(correl)) %>% 
-    filter(transcript != 'refExpr')
-  
-  filteredData = left_join(filteredData, correl, by = "transcript", copy = TRUE) %>% 
-    mutate(transFacet = paste0(gene, '(', transcript, ')'))
+  # Combine the reference data with the normal data.
+  # Remove the reference tissue.
+  # Calculate the fold change
+  filteredData = left_join(filteredData, refExpr, by = 'tissue') %>% 
+    filter(fullName != input$compRef) %>% 
+    mutate(FC = expr / refExpr,
+           logFC = log10(FC))
   
   
-  yMax = max(max(filteredData$refExpr), max(filteredData$expr))
-  yMin = min(min(filteredData$refExpr), min(filteredData$expr))
+  # Calculate correlation coefficient ---------------------------------------
+  
+  # pairwise = spread(filteredData %>% select(tissue, transcript, expr, refExpr),
+  #                   transcript, expr) %>% 
+  #   select(-tissue)
+  # 
+  # correl = data.frame(cor(pairwise)) %>% 
+  #   select(corr = refExpr) 
+  # 
+  # correl = correl %>%
+  #   mutate(transcript = row.names(correl)) %>% 
+  #   filter(transcript != 'refExpr')
+  # 
+  # filteredData = left_join(filteredData, correl, by = "transcript", copy = TRUE) %>% 
+  #   mutate(transFacet = paste0(gene, '(', transcript, ')'))
   
   
-  shading <- data.frame(x1 = c(yMin, yMax, yMin),
-                        y1 = c(yMin, yMax, yMax),
-                        x2 = c(yMin, yMax, yMax),
-                        y2 = c(yMin, yMin, yMax),
-                        group = c(1, 1, 1))
+  # Calculate limits for the plot
+  yMax = max(abs(filteredData$logFC))
   
+  glimpse(filteredData)
   
-
+  # Refactorize -------------------------------------------------------------
   
-  x = ggplot(filteredData, aes(x = expr, y = refExpr,
-                               colour = tissue)) +
-    geom_polygon(aes(x = x1, y = y1, group = group), 
-                 fill = grey15K,  colour = NA,
-                 data = shading, alpha  = 0.3) +
-    geom_polygon(aes(x = x2, y = y2, group = group), 
-                 fill = grey30K,  colour = NA,
-                 data = shading, alpha = 0.3) +
-    geom_abline(slope = 1, intercept = 0,
-                colour = grey40K, size = 0.5,
-                linetype = 2) +
-    annotate(geom = 'text', x = yMin, y = yMax * 0.95,
-             hjust = 0, 
-             label = 'lower expression than ref.', colour = grey70K, size = 3) +
-    annotate(geom = 'text', x = yMax * 0.95, y = yMin*1.05,
-             hjust = 1,
-             label = 'higher expression than ref.', colour = grey90K, size = 3) +
-    # geom_smooth(method = "lm", se = FALSE, 
-    # colour = grey40K, size = 0.5) +
-    geom_point(size = 4) +
-    facet_wrap(~transFacet) +
-    ylab('ref. - uc007afc') +
-    theme_bw() +
-    theme(
-      text = element_text(colour = grey80K),
-      plot.title = element_text(hjust = 0),
-      rect = element_blank(),
-      plot.background = element_blank(),
-      axis.text = element_text(size = 12,  color = grey80K),
-      title =  element_text(size = 15, hjust = 0, color = grey90K),
-      axis.title =  element_text(size = 14,  color = grey80K, hjust = 0.5, vjust = -0.25),
-      strip.text = element_text(size=14, face = 'bold', hjust = 0.05, vjust = -2.5, color = grey70K),
-      legend.position = 'none',
-      strip.background = element_blank(),
-      axis.ticks = element_blank(),
-      panel.margin = unit(3, 'lines'),
-      panel.grid.major.y = element_line(size = 0.2, color = grey80K),
-      panel.grid.minor.y = element_blank(),
-      panel.grid.minor.x = element_blank(),
-      panel.grid.major.x = element_line(size = 0.2, color = grey80K),
-      axis.title.x = element_blank()) +
-    coord_cartesian(xlim = c(yMin, yMax), ylim = c(yMin, yMax)) +
-    scale_colour_manual(values = 
-                          c('total aorta' = '#b15928',
-                            'thoracic aorta' = '#ffff99',
-                            'AA' = '#fed976',
-                            'atria' = '#ff7f00',
-                            'left ventricle' = '#e31a1c',
-                            'right ventricle' = '#fb9a99',
-                            'diaphragm' = '#6a3d9a',
-                            'eye' = '#cab2d6',
-                            'EDL' = '#1f78b4',
-                            'FDB' = '#a6cee3',
-                            'masseter' = '#1d91c0',
-                            'plantaris' = '#7bccc4',
-                            'soleus' = '#33a02c',
-                            'tongue' = '#b2df8a'))
+  # Reverse tissue names
+  filteredData$tissue = factor(filteredData$tissue, levels = rev(levels(filteredData$tissue)))
   
   
   
-  ggplotly(x)
+  # Plot --------------------------------------------------------------------
+  
+  ggplot(filteredData,
+         aes(x = logFC, xend = 0, y = tissue, yend = tissue,
+             fill = logFC)) +
+    geom_segment(colour = grey40K, size = 0.25) +
+    geom_vline(xintercept = 0, colour = grey90K, size = 0.25) +
+    geom_point(size = 4, colour = grey70K,
+               shape = 21) +
+    scale_fill_gradientn(colours = brewer.pal(10, 'RdYlBu'),
+                         limits = c(-yMax, yMax)) +
+    theme_xgrid() +
+    theme(rect = element_rect(colour = grey90K, size = 0.25, fill = NA),
+          panel.border = element_rect(colour = grey90K, size = 0.25, fill = NA)) +
+    facet_wrap(~fullName) +
+    xlab('log(fold change)')
   
 })
-# # 
-# #   # Temporary plgot to be replaced by interactive version.
-# #   filteredData %>%   
-# #     group_by(transcript) %>% 
-# #     ggvis(x = ~tissue, y = ~expr2, colour =: ~transcript) %>%
-# #     layer_bars()
-# # })%>% bind_shiny("compPlot")
-# # 
-# # # 
-# mtcars %>%   ggvis(~mpg, ~wt) %>%
-#   layer_points() %>%
-#   layer_smooths() %>%
-#   bind_shiny("compPlot")
-# 
-# 
-# # n= n %>% mutate(expr1 = n/maxN)
-# 
-# # n %>% ggvis(x=~c(1:8), y=~expr/maxN) %>% 
-# #   layer_bars(fill:="dodgerblue") %>%
-# #   scale_numeric("y", domain = c(0, 1)) %>%
-# #                   hide_axis("y") %>%
-# #                   hide_axis("x")
+
+
