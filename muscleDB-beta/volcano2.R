b8785b8f7c4ce139402711f2909fa16fdad74b4b
flaneuse <laura.d.hughes@gmail.com>
2016-10-10 15:19:18 -0400

    basic ggvis plots

--- muscleDB-beta/volcano2.R
+++ muscleDB-beta/volcano2.R
@@ -1,13 +1,50 @@
 library(ggvis)
-
-df %>% 
-  filter(!is.na(AA.AOR_q)) %>% 
-  ggvis(~log(AA.AOR_q)) %>% 
-  layer_densities()
-
-
-library(ggvis)
 library(shiny)
+
+alpha_dot = 0.3
+size_dot = 25
+
+
+df = data %>% 
+  filter(tissue %in% c('total aorta', 'abdominal aorta')) %>% 
+  select(id, tissue, transcript, gene, shortName, geneLink, transcriptLink, expr, AA.AOR_q) %>% 
+  spread(tissue, expr) %>% 
+  mutate(FC = signif(`abdominal aorta`/`total aorta`, 3),
+         logFC = signif(log(`abdominal aorta`/`total aorta`), 3),
+         logQ = signif(log(AA.AOR_q), 3))
+
+server <- function(input, output) {
+  df %>% 
+    filter(is.finite(logFC)) %>% 
+    ggvis(~logFC, ~-1*logQ) %>% 
+    layer_points(opacity := alpha_dot,
+                 size := size_dot) %>% 
+    bind_shiny('plot1')
+  
+  df %>% 
+    filter(!is.na(logQ)) %>% 
+    ggvis(~logQ) %>% 
+    layer_densities() %>% 
+    bind_shiny('plot2')
+  
+  df %>% 
+    filter(!is.na(logFC)) %>% 
+    ggvis(~logFC) %>% 
+    layer_densities() %>% 
+    bind_shiny('plot3')
+}
+
+ui <- fluidPage(
+    mainPanel(ggvisOutput("plot1"),
+              ggvisOutput("plot2"),
+              ggvisOutput("plot3"))
+  )
+
+shinyApp(ui = ui, server = server)
+
+
+
+
 
 set.seed(1233)
 cocaine <- cocaine[sample(1:nrow(cocaine), 500), ]
