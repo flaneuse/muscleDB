a2c1514844090e983866aa725f2670cf3b1d7f08
flaneuse <laura.d.hughes@gmail.com>
2017-02-19 17:21:11 -0500

    update to 17 tissues.

--- prep/2017-02-19_ANOVAs.R
+++ prep/2017-02-19_ANOVAs.R
@@ -1,5 +1,4 @@
-# Import function to loop through the ANOVA calculations.
-source('ANOVAlookupTable.r')
+# Overall wrapper function to calculate ANOVAs for muscle tissues.
 
 # For ANOVAs, don't run through EVERY ANOVA calc; merely run through:
 # 1. all tissues
@@ -10,117 +9,111 @@ source('ANOVAlookupTable.r')
 # 6. all cardiac muscles
 
 
+# Import function to loop through the ANOVA calculations.
+setwd('~/GitHub/muscleDB/prep/')
+source('ANOVAlookupTable.r')
+library(readxl) # reading in excel files.
+library(dplyr)
+
 # define muscle codes -----------------------------------------------------
-skel = c()
-smooth = c()
-striated = c()
-cardiac = c()
-allMusc = c(skel, smooth, striated, cardiac)
+skelMuscles = c('DIA', 'EDL', 'EYE', 'SOL', 'TON','FDB', 'MAS', 'PLA', 'TAN', 'QUAD', 'GAS')
+smoothMuscles = c('TA', 'AA', 'AOR')
+cardiacMuscles = c('ATR', 'LV', 'RV')
+striatedMuscles = c(cardiacMuscles, skelMuscles)
+allMusc = c(skelMuscles, smoothMuscles, cardiacMuscles)
 
 
-anovas = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE)
-saveRDS(anovas, 'pairwiseANOVAs_2015-10-18.rds')
-write.csv(anovas, 'pairwiseANOVAs_2015-10-18.csv')
+# define raw data file ----------------------------------------------------
+# rawDataFile = '~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv'
+rawDataFile = '~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants (Feb 2017 update).xlsx'
+outputDir = '~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/ANOVAs/'
 
-anovas = anovas[,1:182]
-write.csv(anovas, 'pairwiseANOVAs_trimmed_2015-10-18.csv')
-saveRDS(anovas, 'pairwiseANOVAs_trimmed_2015-10-18.rds')
 
-# All tissues.
-anovas = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 14)
-saveRDS(anovas, 'allANOVAs_2015-10-18.rds')
-write.csv(anovas, 'allANOVAs_2015-10-18.csv')
+# import raw data  --------------------------------------------------------
+rawdata = read_excel(rawDataFile)
 
-anovas2 = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', 
-                          onlyPairwise = TRUE, n = 10,
-                          muscles = c('ATR', 'LV', 'RV', 'DIA', 'AOR',
-                                      'EDL', 'EYE', 'SOL', 'FDB', 'PLA'))
-saveRDS(anovas2, 'allWebANOVAs_2015-10-18.rds')
-write.csv(anovas2, 'allWebANOVAs_2015-10-18.csv')
+rawdata = rawdata %>% 
+  select(Transcript, Coordinates, Length, contains('MIN_ANTI'))
 
-# Smooth
-smooth = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 3, muscles = c('TA, AA, AOR'))
-smooth = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 3, muscles = c('TA', 'AA', 'AOR'))
-write.csv(smooth, 'allSmoothANOVAs_2015-10-18.csv')
-saveRDS(smooth, 'allSmoothANOVAs_2015-10-18.rds')
+# Convert to a matrix; ANOVAs will only work w/ matrix output
+rawdata = as.matrix(rawdata)
 
-# Cardiac
-card = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 3, muscles = c('ATR', 'LV', "RV"))
-write.csv(card, 'allCardiacANOVAs_2015-10-18.csv')
-saveRDS(card, 'allCardiacANOVAs_2015-10-18.rds')
+# 1. run ALL TISSUES ------------------------------------------------------
+anovas = ANOVAlookupTable(rawdata, muscles = allMusc, onlyPairwise = TRUE, n = length(allMusc))
+saveRDS(anovas, paste0(outputDir, 'allANOVAs_2017-02-19.rds'))
+write.csv(anovas, paste0(outputDir,'allANOVAs_2017-02-19.csv'))
 
-# Skeletal
-skel = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 8, muscles = c('DIA', 'EDL', 'EYE', 'SOL', 'TON','FDB', 'MAS', 'PLA'))
-saveRDS(skel, 'allSkelANOVAs_2015-10-18.rds')
-write.csv(skel, 'allSkelANOVAs_2015-10-18.csv')
 
-skel = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 6, muscles = c('DIA', 'EDL', 'EYE', 'SOL','FDB', 'PLA'))
-saveRDS(skel, 'SkelANOVAs_2015-10-18.rds')
-write.csv(skel, 'skelANOVAs_2015-10-18.csv')
+# 2. run ALL PAIRWISE -----------------------------------------------------
 
-# Striated
-striated = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 11, muscles = c('ATR', 'LV', 'RV', 'DIA', 'EDL', 'EYE', 'SOL', 'TON','FDB', 'MAS', 'PLA'))
-write.csv(striated, 'allStriatedANOVAs_2015-10-18.csv')
-saveRDS(striated, 'allStriatedANOVAs_2015-10-18.rds')
+anovas = ANOVAlookupTable(rawdata, onlyPairwise = TRUE, muscles = allMusc, n = 2)
+saveRDS(anovas, paste0(outputDir,'pairwiseANOVAs_2017-02-19.rds'))
+write.csv(anovas, paste0(outputDir,'pairwiseANOVAs_2017-02-19.csv'))
 
-striated = ANOVAlookupTable('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants.csv', onlyPairwise = TRUE, n = 9, muscles = c('ATR', 'LV', 'RV', 'DIA', 'EDL', 'EYE', 'SOL', 'FDB', 'PLA'))
-saveRDS(striated, 'striatedANOVAs_2015-10-18.rds')
-write.csv(striated, 'striatedANOVAs_2015-10-18.csv')
+
+# 3. SMOOTH ---------------------------------------------------------------
+smooth = ANOVAlookupTable(rawdata, onlyPairwise = TRUE, n = length(smoothMuscles), muscles = smoothMuscles)
+write.csv(smooth, paste0(outputDir,'allSmoothANOVAs_2017-02-19.csv'))
+saveRDS(smooth, paste0(outputDir,'allSmoothANOVAs_2017-02-19.rds'))
+
+
+
+# 4. CARDIAC --------------------------------------------------------------
+
+card = ANOVAlookupTable(rawdata, onlyPairwise = TRUE, n = length(cardiacMuscles), muscles = cardiacMuscles)
+write.csv(card, paste0(outputDir,'allCardiacANOVAs_2017-02-19.csv'))
+saveRDS(card, paste0(outputDir,'allCardiacANOVAs_2017-02-19.rds'))
+
+
+# 5. SKELETAL -------------------------------------------------------------
+
+skel = ANOVAlookupTable(rawdata, onlyPairwise = TRUE, n = length(skelMuscles), muscles = skelMuscles)
+saveRDS(skel, paste0(outputDir,'allSkelANOVAs_2017-02-19.rds'))
+write.csv(skel, paste0(outputDir,'allSkelANOVAs_2017-02-19.csv'))
+
+
+# 6. STRIATED -------------------------------------------------------------
+
+striated = ANOVAlookupTable(rawdata, onlyPairwise = TRUE, n = length(striatedMuscles), muscles = striatedMuscles)
+write.csv(striated, paste0(outputDir,'allStriatedANOVAs_2017-02-19.csv'))
+saveRDS(striated, paste0(outputDir,'allStriatedANOVAs_2017-02-19.rds'))
+
+
+
+
+# MERGE DATA --------------------------------------------------------------
 
 
 # Merge all together.
-anovas = readRDS('allANOVAs_2015-10-18.rds')
+anovas = readRDS(paste0(outputDir, 'allANOVAs_2017-02-19.rds'))
 anovas = data.frame(transcript = row.names(anovas), anovas)
 
-anovasWeb = readRDS('allWebANOVAs_2015-10-18.rds')
-anovasWeb = data.frame(transcript = row.names(anovasWeb), anovasWeb)
-
-
-striatedWeb = readRDS('striatedANOVAs_2015-10-18.rds')
-striatedWeb = data.frame(transcript = row.names(striatedWeb), striatedWeb)
-striated = readRDS('allStriatedANOVAs_2015-10-18.rds')
+striated = readRDS(paste0(outputDir,'allStriatedANOVAs_2017-02-19.rds'))
 striated = data.frame(transcript = row.names(striated), striated)
 
-cardiac = readRDS('allCardiacANOVAs_2015-10-18.rds')
+cardiac = readRDS(paste0(outputDir,'allCardiacANOVAs_2017-02-19.rds'))
 cardiac = data.frame(transcript = row.names(cardiac), cardiac)
 
-skeletalWeb = readRDS('SkelANOVAs_2015-10-18.rds')
-skeletalWeb = data.frame(transcript = row.names(skeletalWeb), skeletalWeb)
-skeletal = readRDS('allSkelANOVAs_2015-10-18.rds')
+skeletal = readRDS(paste0(outputDir,'allSkelANOVAs_2017-02-19.rds'))
 skeletal = data.frame(transcript = row.names(skeletal), skeletal)
 
-smooth = readRDS('allSmoothANOVAs_2015-10-18.rds')
+smooth = readRDS('allSmoothANOVAs_2017-02-19.rds')
 smooth = data.frame(transcript = row.names(smooth), smooth)
 
 
-pairwise = readRDS('pairwiseANOVAs_trimmed_2015-10-18.rds')
+pairwise = readRDS(paste0(outputDir,'pairwiseANOVAs_trimmed_2017-02-19.rds'))
 pairwise = data.frame(transcript = row.names(pairwise), pairwise)
 
 
-library(dplyr)
+# Merge files together ----------------------------------------------------
+
+
 allANOVAs = full_join(pairwise, smooth)
 allANOVAs = full_join(allANOVAs, cardiac)
 allANOVAs = full_join(allANOVAs, skeletal)
-allANOVAs = full_join(allANOVAs, skeletalWeb)
 allANOVAs = full_join(allANOVAs, striated)
-allANOVAs = full_join(allANOVAs, striatedWeb)
 allANOVAs = full_join(allANOVAs, anovas)
-allANOVAs = full_join(allANOVAs, anovasWeb)
 
-write.csv(allANOVAs, 'allANOVAs_merged_2015-10-18.csv')
-saveRDS(allANOVAs, 'allANOVAs_merged_2015-10-18.rds')
+write.csv(allANOVAs, paste0(outputDir,'allANOVAs_merged_2017-02-19.csv'))
+saveRDS(allANOVAs, paste0(outputDir,'allANOVAs_merged_2017-02-19.rds'))
 
-
-ANOVAs4Web = allANOVAs %>% 
-  select(-contains('TA'), -contains('AA'), -contains('MAS'), -contains('TON'))
-
-write.csv(ANOVAs4Web, 'allANOVAs_forWeb_2015-10-18.csv')
-saveRDS(ANOVAs4Web, 'allANOVAs_forWeb_2015-10-18.rds')
-
-
-ANOVAs4Web = allANOVAs %>% 
-  select(-contains('_p'),
-         -contains('TA'), -contains('AA'), 
-         -contains('MAS'), -contains('TON'))
-
-saveRDS(ANOVAs4Web, 'q_forWeb_2015-10-18.rds')
