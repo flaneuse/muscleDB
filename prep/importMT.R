ce4cc50ed80d46039d4b211c747f19f84a91a4ff
flaneuse <laura.d.hughes@gmail.com>
2017-04-16 19:47:24 -0400

    update ANOVAs, GO

--- prep/importMT.R
+++ prep/importMT.R
@@ -15,11 +15,12 @@ library(readxl)
 
 # Import averages and SEs. ------------------------------------------------
 # df <- read.delim("~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/Expression_Levels_for_webpage(10-6-15).txt")
-df <- read_excel("~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants (Feb 2017 update).xlsx")
+# df <- read_excel("~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/Muscle_Re-run_Mapstats_Quantfiles/MT_adjusted_TranscriptQuants (Feb 2017 update).xlsx")
+df = read.csv('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/Fixed SEs TAN QUAD GAS.csv')
 
 # Filter out any NA values
 df = df %>% 
-  filter(!is.na(Transcript))
+  filter(!is.na(Transcript), Transcript != "")
 
 
 numTranscripts = nrow(df)
@@ -27,7 +28,6 @@ numTranscripts = nrow(df)
 # Pull out the averages and gather ----------------------------------------
 avg = df %>% 
   select(transcript = Transcript, 
-         
          # transcript means
          atria = ATR_mean, `left ventricle` = LV_mean,
          `total aorta` = AOR_mean, `right ventricle` = RV_mean,
@@ -79,23 +79,37 @@ SE = df %>%
 
 
 # Pull in the outdated data to get GO and gene names ----------------------
-oldFile = '~/Dropbox/Muscle Transcriptome Atlas/Website files/MTapp-v0-51/data/combData_2014-10-19.rds'
+# oldFile = '~/Dropbox/Muscle Transcriptome Atlas/Website files/MTapp-v0-51/data/combData_2014-10-19.rds'
 
-geneInfo = read_rds(oldFile) %>% 
-  select(Transcript, shortName, geneSymbol, geneName, GO, EntrezLink, UCSCLink) %>% 
-  mutate(uc = str_extract(Transcript, 'uc......'), # Remove extra crap from transcript ids
-         NM = str_extract(Transcript, 'N...............')) %>% 
-  mutate(transcript = ifelse(is.na(uc), NM, uc), # tidying up transcript name.
-         
-         geneLink = ifelse(geneSymbol == "", # Gene symbol w/ link to entrez page.
-                           "", paste0("<a href = '", EntrezLink, 
-                                      "' target = '_blank'>", geneSymbol, "</a>")), 
-         transcriptLink = ifelse(UCSCLink == "",
-                                 transcript, 
-                                 paste0("<a href = '", UCSCLink,
-                                        "' target = '_blank'>", transcript, "</a>")) # transcript name w/ link to UCSC page
-  ) %>% 
-  select(transcript, shortName, gene = geneSymbol, GO, geneLink, transcriptLink)
+# Updated by Scott 
+ont_file = '~/Dropbox/Muscle Transcriptome Atlas/Website files/data/MOUSE_RUM_Transcripts+.xlsx'
+
+geneInfo = read_excel(ont_file) %>% 
+  filter(!is.na(GO)) %>%
+  select(transcript, geneSymbol, GO, EntrezLink, UCSCLink) %>% 
+  # mutate(uc = str_extract(Transcript, 'uc......'), # Remove extra crap from transcript ids
+         # NM = str_extract(Transcript, 'N...............')) %>% 
+  # mutate(transcript = ifelse(is.na(uc), NM, uc), # tidying up transcript name.
+  
+  # mutate(geneLink = ifelse(geneSymbol == "", # Gene symbol w/ link to entrez page.
+                           # "", paste0("<a href = '", EntrezLink, 
+                                      # "' target = '_blank'>", geneSymbol, "</a>")), 
+         # transcriptLink = ifelse(UCSCLink == "",
+                                 # transcript, 
+                                 # paste0("<a href = '", UCSCLink,
+                                        # "' target = '_blank'>", transcript, "</a>")) # transcript name w/ link to UCSC page
+  # ) %>% 
+  select(transcript, gene = geneSymbol, GO, geneLink = EntrezLink, transcriptLink = UCSCLink)
+
+# save ontology
+write_rds(geneInfo, '~/Dropbox/Muscle Transcriptome Atlas/Website files/data/geneOntology_2017-04.rds')
+
+# save master list of ontology terms for website
+ont_terms = stringr::str_split(geneInfo$GO, pattern = '\\|')
+
+ont_terms = unique(unlist(ont_terms))
+
+write_rds(ont_terms, '~/Dropbox/Muscle Transcriptome Atlas/Website files/data/allOntologyTerms.rds')
 
 # Merge everything together -----------------------------------------------
 df = full_join(avg, SE, by = c("transcript", "tissue")) %>% 
@@ -103,20 +117,27 @@ df = full_join(avg, SE, by = c("transcript", "tissue")) %>%
     lb = expr - SE,
     ub = expr + SE)
 
+if(nrow(df) != nrow(avg) | nrow(df) != nrow(SE)) {
+  warning("Merge wasn't successful")
+}
 
 
 # Merge in ANOVAs ---------------------------------------------------------
-anovas = readRDS('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/ANOVAs/allANOVAs_merged_2017-02-19.rds')
+anovas = readRDS('~/Dropbox/Muscle Transcriptome Atlas/RUM_Re-analysis/ANOVAs/allANOVAs_merged_2017-04-15.rds')
 
 anovas = anovas %>% 
-  select(-transcript.1) %>% 
+  # select(-transcript.1) %>% 
   group_by(transcript) %>% 
+  select(-contains('_p')) %>% 
   mutate_each(funs(signif(., digits = 3)))
 
 
 # Merge
-df = full_join(df, anovas, by = 'transcript') %>% 
-  select(-contains('_p'))
+df = full_join(df, anovas, by = 'transcript')
+  
+if(nrow(df) != nrow(avg) | nrow(df) != nrow(SE)) {
+  warning("Merge wasn't successful")
+}
 
 # Clean the transcript IDs into shortened versions ------------------------
 df = df %>% mutate(uc = str_extract(df$transcript, 'uc......'),
@@ -131,8 +152,11 @@ df = df %>% mutate(uc = str_extract(df$transcript, 'uc......'),
 
 
 # Merge in GO, ontology
-df = left_join(df, 
-                      geneInfo, by = c("transcript" = "transcript"))
+df = left_join(df, geneInfo, by = c("transcript" = "transcript"))
+
+# create shortened ID
+df = df %>% 
+  mutate(shortName = ifelse(is.na(gene), transcript, gene))
 
 
 # refactorise the tissue levels.
@@ -160,8 +184,8 @@ df$tissue = factor(df$tissue,
 
 # save files --------------------------------------------------------------
 
-saveRDS(df, '~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_2017-04-02.rds')
-
+saveRDS(df, '~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_2017-04-16.rds')
+write_csv(df, '~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_2017-04-16.csv')
 
 # Copy to sqlite db -------------------------------------------------------
 # db = src_sqlite('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_public_2017-02-19.sqlite3',
